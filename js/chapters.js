d05f85dff0b1d11ffee57e6168b87c254bebb513
hacker12345227 <Jarno.eulen12@gmail.com>
2026-03-16 12:16:11 +0100

    Update and rename chapters.js to js/chapters.js

---
+++ js/chapters.js
@@ -0,0 +1,78 @@
+/* 
+Hoofdstukken voor de lessen.
+De woorden hier moeten ook bestaan in de dictionary.json
+*/
+
+const chapters = [
+
+{
+id: 1,
+title: "Voorstellen",
+description: "Basiswoorden om jezelf voor te stellen",
+
+words: [
+"ik",
+"jij",
+"hij",
+"wij",
+"jullie",
+"zij",
+"naam",
+"hallo",
+"doei",
+"persoon",
+"vriend"
+]
+
+},
+
+{
+id: 2,
+title: "Plaatsen",
+description: "Woorden over plekken",
+
+words: [
+"huis",
+"stad",
+"school",
+"kamer",
+"straat",
+"gebouw"
+]
+
+},
+
+{
+id: 3,
+title: "Werkwoorden",
+description: "Veelgebruikte werkwoorden",
+
+words: [
+"gaan",
+"komen",
+"zien",
+"eten",
+"lopen",
+"spreken"
+]
+
+},
+
+{
+id: 4,
+title: "Basiswoorden",
+description: "Algemene woorden",
+
+words: [
+"ja",
+"nee",
+"dank",
+"goed",
+"slecht",
+"groot",
+"klein"
+]
+
+}
+
+]
